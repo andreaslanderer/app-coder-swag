8b827fbdc02783a968307feb7b9f02a1230712d5
Andreas Landerer <andreas.landerericloud.com>
2017-11-27 22:59:36 +0100

    Added products view

---
+++ app-coder-swag/View/ProductCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCollectionViewCell.swift
+//  app-coder-swag
+//
+//  Created by Andreas Landerer on 27.11.17.
+//  Copyright © 2017 Zuehlke. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
